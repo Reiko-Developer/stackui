1a31390046353bbe78ef66badbc05140454e3f60
Vitor Lucas Pires Cordovil <lukassfifa@gmail.com>
2020-10-28 15:30:27 -0300

    Added a custom Drawer made by MarcinusX

--- lib/widget/custom_drawer.dart
+++ lib/widget/custom_drawer.dart
@@ -9,37 +9,46 @@ class CustomDrawer extends StatefulWidget {
 
 class _CustomDrawerState extends State<CustomDrawer>
     with SingleTickerProviderStateMixin {
-  AnimationController _animController;
-  final double _maxSlide = 200;
+  //properties
+  static const Duration toggleDuration = Duration(milliseconds: 250);
+
+  static const double _maxSlide = 200;
+  static const double minDragStartEdge = 60;
+  static const double maxDragStartEdge = _maxSlide - 16;
+  AnimationController _animationController;
+  bool _canBeDragged = false;
+
+  void close() => _animationController.reverse();
+
+  void open() => _animationController.forward();
+
+  void toggleDrawer() => _animationController.isCompleted ? close() : open();
 
   @override
   initState() {
     super.initState();
-    _animController = AnimationController(
+    _animationController = AnimationController(
       vsync: this,
-      duration: Duration(milliseconds: 250),
+      duration: toggleDuration,
     );
   }
 
-  void toggle() {
-    _animController.isDismissed
-        ? _animController.forward()
-        : _animController.reverse();
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
   }
 
-  // void _onDragStart(DragStartDetails details) {
-  //   bool isDragOpenFromLeft =
-  //       _animController.isDismissed && details.globalPosition.dx;
-  // }
-
-  // void _onDragUpdate() {}
-  // void _onDragEnd() {}
+  void toggle() {
+    _animationController.isDismissed
+        ? _animationController.forward()
+        : _animationController.reverse();
+  }
 
   @override
   Widget build(BuildContext context) {
-    var myDrawer = Container(color: Colors.blue);
-    var myChild = Container(color: Colors.yellow);
-
+    var _myChild = Container(color: Colors.yellow);
+    var _myDrawer = MyDrawer();
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blue,
@@ -57,27 +66,112 @@ class _CustomDrawerState extends State<CustomDrawer>
         width: 400,
         child: GestureDetector(
           onTap: toggle,
-          // onHorizontalDragStart: _onDragStart,
-          // onHorizontalDragUpdate: _onDragUpdate,
-          // onHorizontalDragEnd: _onDragEnd,
+          onHorizontalDragStart: _onDragStart,
+          onHorizontalDragUpdate: _onDragUpdate,
+          onHorizontalDragEnd: _onDragEnd,
           child: AnimatedBuilder(
-            animation: _animController,
+            animation: _animationController,
             builder: (context, _) {
-              double slide = _maxSlide * _animController.value;
-              double scale = 1 - (_animController.value * 0.3);
+              double slide = _maxSlide * _animationController.value;
+              double scale = 1 - (_animationController.value * 0.3);
               return Stack(
                 children: <Widget>[
-                  myDrawer,
+                  _myDrawer,
                   Transform(
                     transform: Matrix4.identity()
                       ..translate(slide)
                       ..scale(scale),
                     alignment: Alignment.centerLeft,
-                    child: myChild,
+                    child: _myChild,
                   ),
                 ],
               );
             },
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _onDragStart(DragStartDetails details) {
+    bool isDragOpenFromLeft = _animationController.isDismissed &&
+        details.globalPosition.dx < minDragStartEdge;
+    bool isDragCloseFromRight = _animationController.isCompleted &&
+        details.globalPosition.dx > maxDragStartEdge;
+
+    _canBeDragged = isDragOpenFromLeft || isDragCloseFromRight;
+  }
+
+  void _onDragUpdate(DragUpdateDetails details) {
+    if (_canBeDragged) {
+      double delta = details.primaryDelta / _maxSlide;
+      _animationController.value += delta;
+    }
+  }
+
+  void _onDragEnd(DragEndDetails details) {
+    //I have no idea what it means, copied from Drawer
+    double _kMinFlingVelocity = 365.0;
+
+    if (_animationController.isDismissed || _animationController.isCompleted) {
+      return;
+    }
+    if (details.velocity.pixelsPerSecond.dx.abs() >= _kMinFlingVelocity) {
+      double visualVelocity = details.velocity.pixelsPerSecond.dx /
+          MediaQuery.of(context).size.width;
+
+      _animationController.fling(velocity: visualVelocity);
+    } else if (_animationController.value < 0.5) {
+      close();
+    } else {
+      open();
+    }
+  }
+}
+
+class MyDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.blueAccent,
+      child: SafeArea(
+        child: Theme(
+          data: ThemeData(brightness: Brightness.dark),
+          child: Padding(
+            padding: EdgeInsets.only(top: 10),
+            child: Column(
+              children: [
+                Container(
+                  alignment: Alignment.topLeft,
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 10, left: 15),
+                    child: Image.asset(
+                      'assets/images/smallwarrior.jpg',
+                    ),
+                  ),
+                ),
+                ListTile(
+                  leading: Icon(Icons.new_releases),
+                  title: Text('News'),
+                ),
+                ListTile(
+                  leading: Icon(Icons.star),
+                  title: Text('Favourites'),
+                ),
+                ListTile(
+                  leading: Icon(Icons.map),
+                  title: Text('Map'),
+                ),
+                ListTile(
+                  leading: Icon(Icons.settings),
+                  title: Text('Settings'),
+                ),
+                ListTile(
+                  leading: Icon(Icons.person),
+                  title: Text('Profile'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
